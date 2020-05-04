f69b6d24a992b3524ac96e1a586092bbbf8f30bb
Shubham Upadhyay <49741686+shubhamdec@users.noreply.github.com>
2020-05-04 13:48:21 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+    $nav = $('.nav');
+    $toggleCollapse =  $('.toggle-collapse');
+
+    /* Click event on toggle menu */
+    $toggleCollapse.click(function() {
+        $nav.toggleClass('collapse');
+    })
+
+    //owl-carousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        dots:false,
+        nav:true,
+    });
+
+});
+
